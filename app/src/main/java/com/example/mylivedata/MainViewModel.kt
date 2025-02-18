6ee2fbc30fe6840acd064a8f54801866bfa1fdfd
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-18 09:19:36 +0700

    Menyelesaikan MainViewModel.kt

--- app/src/main/java/com/example/mylivedata/MainViewModel.kt
+++ app/src/main/java/com/example/mylivedata/MainViewModel.kt
@@ -1,4 +1,31 @@
 package com.example.mylivedata
 
-class MainViewModel {
+import android.annotation.SuppressLint
+import android.os.SystemClock
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.util.Timer
+import java.util.TimerTask
+
+@SuppressLint("SuspiciousIndentation")
+class MainViewModel: ViewModel() {
+    companion object{
+        private const val ONE_SECOND = 1000
+    }
+    private val militialTime = SystemClock.elapsedRealtime()
+    private val melapsedTime = MutableLiveData<Long?>()
+
+    init {
+     val timer = Timer()
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                val newValue = (SystemClock.elapsedRealtime() - militialTime) / 1000
+                melapsedTime.postValue(newValue)
+            }
+        }, ONE_SECOND.toLong(), ONE_SECOND.toLong())
+    }
+    fun getElapsedTime(): LiveData<Long?>{
+        return melapsedTime
+    }
 }
